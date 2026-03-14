ab94993dd59ad405e80b814e140cce20079bd9cc
lcl <2251086272@qq.com>
2026-03-14 22:47:08 +0800

    feat: 修改访问路径

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from "vuepress";
 import { plumeTheme } from "vuepress-theme-plume";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/blog/",
   lang: "zh-CN",
   title: "Write-A-Blog",
   description: "Write A Blog",
